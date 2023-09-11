e7936f3acc1871a3ada7e320f18ca2d2c93c6d2d
Juan Elvir <jelvir97@gmail.com>
2023-09-11 16:39:15 -0600

    user model created

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -97,6 +98,10 @@ class Pantry(db.Model):
                                   secondary='ingredient_pantry', 
                                   backref='pantries')
     
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
+    
 class IngredientPantry(db.Model):
     """Many-to-many relation between ingredients and pantries"""
 
@@ -109,3 +114,27 @@ class IngredientPantry(db.Model):
     pantry_id = db.Column(db.Integer,
                         db.ForeignKey('pantries.id'),
                         primary_key=True)
+    
+class User(db.Model, UserMixin):
+    """User class fro login and auth"""
+
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        return f"<User first_name = {self.first_name} last_name={self.last_name}>"
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    last_name = db.Column(db.String(50),
+                     nullable=False)
+    
+    img_url = db.Column(db.String(),
+                     nullable=False,
+                     default='https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png')
+    
+    pantries = db.relationship('Pantry',cascade="all, delete-orphan", backref='user')
