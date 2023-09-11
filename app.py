c43fa71bfa2260cc707b25e972d6bff810ed7801
Juan Elvir <jelvir97@gmail.com>
2023-09-11 08:47:09 -0600

    boilerplate models.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template,flash,get_flashed_messages, request,session, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, User, db, Feedback
-from forms import RegisterForm, LoginForm, AddFeedbackForm, UpdateFeedbackForm
+# from forms import RegisterForm, LoginForm, AddFeedbackForm, UpdateFeedbackForm
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///auth_demo'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql:///virtual_pantry'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = 'mangotreeee'
---
+++ models.py
@@ -0,0 +1,8 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
