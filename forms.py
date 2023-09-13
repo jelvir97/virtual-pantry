5e07af20b0db8c46bca827316cdd9c03db4e5865
Juan Elvir <jelvir97@gmail.com>
2023-09-13 08:17:20 -0600

    forms.py added to repo w/ login and recipe forms added

---
+++ forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms_alchemy import model_form_factory
+from models import db, User, Recipe, Ingredient, Pantry
+
+BaseModelForm = model_form_factory(FlaskForm)
+
+class ModelForm(BaseModelForm):
+    @classmethod 
+    def get_session(self):
+        return db.session
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        only = ['email','password']
+    
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
